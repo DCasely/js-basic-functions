2c20248b40eaf430769eb57611bc9efe72982f73
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-05 13:39:28 -0500

    covered Arguments

--- worksheet.js
+++ worksheet.js
@@ -1,3 +1,5 @@
+// FUNCTIONS
+
 function doSomething() {
   console.log('HELLO WORLD');
 }
@@ -18,3 +20,41 @@ function singSong() {
   console.log('Up above the world so high,');
   console.log('Like a diamond in the sky.');
 }
+
+// ARGUMENTS
+
+function square(num) {
+  console.log(num * num);
+}
+
+square(10);
+square(3);
+square(4);
+
+// -----------------------
+
+function sayHello() {
+  console.log('Hello there!');
+}
+
+function sayHello(name) {
+  console.log('Hello there ' + name + '!');
+}
+
+// MULTIPLE ARGUMENTS
+
+function area(length, width) {
+  console.log(length * width);
+}
+
+area(9, 2);
+
+// ========================
+
+function greet(person1, person2, person3) {
+  console.log('hi ' + person1);
+  console.log('hi ' + person2);
+  console.log('hi ' + person3);
+}
+
+greet('Harry', 'Ron', 'Hermione');
