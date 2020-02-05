04b9b463a4aec86a7b9f59938dabc406653fb3dd
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-05 13:26:50 -0500

    initial commit

---
+++ worksheet.js
@@ -0,0 +1,20 @@
+function doSomething() {
+  console.log('HELLO WORLD');
+}
+
+doSomething();
+doSomething();
+doSomething();
+doSomething();
+
+function sayHi() {
+  console.log('HELLO!');
+  console.log('GOODBYE!');
+}
+
+function singSong() {
+  console.log('Twinkle, twinkle, little star,');
+  console.log('How I wonder what you are!');
+  console.log('Up above the world so high,');
+  console.log('Like a diamond in the sky.');
+}
